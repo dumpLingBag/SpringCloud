fa6bbccc777a793f3cbcdfcdfdde24a7a4674c4a
zpl <992078968@qq.com>
2019-04-18 09:10:32 +0800

    netty webSocket

--- rngay-feign-interface/src/main/java/com/rngay/feign/user/dto/UASaveUserDTO.java
+++ rngay-feign-interface/src/main/java/com/rngay/feign/user/dto/UASaveUserDTO.java
@@ -1,7 +1,6 @@
 package com.rngay.feign.user.dto;
 
 import com.rngay.feign.dto.CommonDTO;
-import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Table;
 import javax.validation.constraints.*;
