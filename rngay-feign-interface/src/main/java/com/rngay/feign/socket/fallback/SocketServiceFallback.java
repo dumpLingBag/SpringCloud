c0b403501a588dd763855c92da844f04dba1170d
zpl <992078968@qq.com>
2019-05-20 14:54:00 +0800

    更新webSocket

--- rngay-feign-interface/src/main/java/com/rngay/feign/socket/fallback/SocketServiceFallback.java
+++ rngay-feign-interface/src/main/java/com/rngay/feign/socket/fallback/SocketServiceFallback.java
@@ -2,7 +2,6 @@ package com.rngay.feign.socket.fallback;
 
 import com.rngay.common.vo.Result;
 import com.rngay.feign.dto.PageQueryDTO;
-import com.rngay.feign.socket.dto.ContentDTO;
 import com.rngay.feign.socket.dto.PageMessageDTO;
 import com.rngay.feign.socket.service.SocketService;
 import feign.hystrix.FallbackFactory;
@@ -12,11 +11,6 @@ public class SocketServiceFallback implements FallbackFactory<SocketService> {
     public SocketService create(Throwable throwable) {
         String result = "消息服务异常，请稍后再试";
         return new SocketService() {
-            @Override
-            public Result<?> sendUser(ContentDTO contentDTO) {
-                return Result.failMsg(result);
-            }
-
             @Override
             public Result<?> sendAll(String content) {
                 return Result.failMsg(result);
